9ab8158dda00006d3f5b564219694d0c2e38e6f6
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-08-15 00:18:35 +0500

    Описание сущности Book

---
+++ Domain/BookStore.Domain/Book.cs
@@ -0,0 +1,14 @@
+﻿namespace BookStore.Domain
+{
+    public class Book
+    {
+        public int Id { get; }
+        public string Title { get; }
+
+        public Book(int id, string title)
+        {
+            Id = id;
+            Title = title;
+        }
+    }
+}
