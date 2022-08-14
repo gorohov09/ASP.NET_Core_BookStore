fafdfb9ddf4f3579e734d4801edc5e75040f014d
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-08-15 00:39:46 +0500

    Создание реализации репозитория в памяти

---
+++ Infrastructure/BookStore.Memory/BookRepository.cs
@@ -0,0 +1,21 @@
+﻿using BookStore.Domain;
+using System.Linq;
+
+namespace BookStore.Memory
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly Book[] _books = new[]
+        {
+            new Book(1, "Art Of Programming"),
+            new Book(2, "Refactoring"),
+            new Book(3, "C Programming Language"),
+        };
+
+        public Book[] GetAllByTitle(string titlePart)
+        {
+            return _books.Where(book => book.Title.Contains(titlePart))
+                .ToArray();
+        }
+    }
+}
