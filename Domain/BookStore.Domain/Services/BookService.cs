9a6a4c5b2f6c0a407dd57b7808267e0c80cbc2ae
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-08-15 20:19:21 +0500

    Заготовка сервиса, для работы с книгами

---
+++ Domain/BookStore.Domain/Services/BookService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Domain.Services
+{
+    public class BookService
+    {
+        public Book[] GetAllByQuery(string query)
+        {
+            return null;
+        }
+    }
+}
