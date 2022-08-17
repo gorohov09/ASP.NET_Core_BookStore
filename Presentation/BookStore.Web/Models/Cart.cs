10cdc9c4c96c82f05534b4ff2b36b71d58226c49
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-08-17 21:31:36 +0500

    Создание контроллера для корзины

--- Presentation/BookStore.Web/Models/Cart.cs
+++ Presentation/BookStore.Web/Models/Cart.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace BookStore.Web.Models
 {
@@ -8,5 +9,7 @@ namespace BookStore.Web.Models
         public IDictionary<int, int> Items { get; set; } = new Dictionary<int, int>();
 
         public decimal TotalSum { get; set; }
+
+        public int TotalCount => Items.Sum(x => x.Value);
     }
 }
