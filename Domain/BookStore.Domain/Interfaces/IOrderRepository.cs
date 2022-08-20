c20e5957fa5053d8afc30bfc7fbf37b0230853ba
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-08-20 17:36:03 +0500

    Репозиторий для работы с заказами

---
+++ Domain/BookStore.Domain/Interfaces/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using BookStore.Domain.Entities;
+
+namespace BookStore.Domain.Interfaces
+{
+    public interface IOrderRepository
+    {
+        Order Create();
+
+        Order GetById(int id);
+
+        void Update(Order order);
+    }
+}
---
+++ Infrastructure/BookStore.Memory/OrderRepository.cs
@@ -0,0 +1,31 @@
+﻿using BookStore.Domain.Entities;
+using BookStore.Domain.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BookStore.Memory
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly List<Order> _orders = new List<Order>();
+        public Order Create()
+        {
+            var nextId = _orders.Count + 1;
+            var order = new Order(nextId, new OrderItem[0]);
+
+            _orders.Add(order);
+
+            return order;
+        }
+
+        public Order GetById(int id)
+        {
+            return _orders.FirstOrDefault(x => x.Id == id);
+        }
+
+        public void Update(Order order)
+        {
+            ;
+        }
+    }
+}
